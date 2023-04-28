d33cb71a7827d33da4044a85cf828f661b9796b6
Narender <113188277+Narender79@users.noreply.github.com>
2023-04-28 15:37:42 +0530

    Add files via upload

---
+++ new_cal.py
@@ -0,0 +1,12 @@
+user=input("what you want to do: ")
+num1=int(input("Enter first number: "))
+num2=int(input("Enter the second number: "))
+
+if user=="+":
+    print("The sum o two no. is: ",num1+num2)
+elif user=="-":
+    print("The subtraction of two number is: ",num1-num2)
+elif user=="*":
+    print("The multiplication of two no. is: ",num1*num2)
+elif user=="/":
+    print("The divide of two number is: ",num1/num2)
